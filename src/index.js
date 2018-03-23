05ad022cd0d50de0813121a18b9dd27c4433bec8
Ke Nguyen <ke.nguyen@2359media.com>
2018-03-23 11:40:44 +0700

    chore(dev): add redux logger

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import 'semantic-ui-css/semantic.min.css';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import rootReducer from './reducers';
 
 const store = createStore(
   rootReducer,
-  applyMiddleware(thunk)
+  applyMiddleware(thunk),
+  applyMiddleware(logger),
 );
 
 import App from './App';
