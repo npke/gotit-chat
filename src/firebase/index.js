deef5e4fe0b3c9e0a21bdaa81718fef9782a203e
Ke Nguyen <ke.nguyen@2359media.com>
2018-03-23 10:36:09 +0700

    feat: config firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,17 @@
+import Firebase from 'firebase';
+import Firestore from 'firebase/firestore';
+
+const config = {
+  apiKey: "AIzaSyANfreqbZObQPOk9FNuYhUBYNp-sZtx-DE",
+  authDomain: "gotit-chat.firebaseapp.com",
+  databaseURL: "https://gotit-chat.firebaseio.com",
+  projectId: "gotit-chat",
+  storageBucket: "gotit-chat.appspot.com",
+  messagingSenderId: "263582348688"
+};
+
+const App = Firebase.initializeApp(config);
+const Auth = App.auth();
+const Database = App.firestore();
+
+export { App, Auth, Database };
