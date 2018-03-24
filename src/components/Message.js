9fe435b6022201583daf4625e161c47a32cd8b8e
Ke Nguyen <ke.nguyen@2359media.com>
2018-03-25 01:07:23 +0700

    feat(message): recognize and display links in message

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './Message.css';
 
+const formatMessage = (message) => {
+  return message.replace(/(https?:\/\/[^\s]+)/gi, "<a target='_blank' href='$1'>$1</a>");
+}
+
 const Message = ({ content, from }) => {
   const messageClassNames = ["message-container", from === "partner" ? "message-container-left" : from === "system" ? "system-message" : "message-container-right"];
   return (
     <div className={messageClassNames.join(" ")}>
       <div className="message-item">
-        {content}
+       <span dangerouslySetInnerHTML={{__html: formatMessage(content)}} />
       </div>
     </div>
   )
