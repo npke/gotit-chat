a0ffe851c7dcf3abeea471953d89614beca97b91
Ke Nguyen <ke.nguyen@2359media.com>
2018-03-24 16:45:10 +0700

    feat(chat): keep message container scrolling to bottom

--- src/containers/Conversation.js
+++ src/containers/Conversation.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Icon } from 'semantic-ui-react';
 
@@ -6,29 +6,48 @@ import "./Conversation.css";
 import ConversationActions from '../actions/conversation';
 import Message from '../components/Message';
 
-const Conversation = ({ partner, messages, onCloseConversation }) => (
-  <div className="conversation"> 
-    <div>
-      <div className="conversation-header">
-        <div className="partner-info">
-          <img className="user-profile-photo" src={partner.profilePhotoUrl} />
-          <h3 className="user-title">{partner.name}</h3>
+class Conversation extends Component {
+
+  scrollToBottom () {
+    const messageContainer = document.getElementById('conversation-body');
+    messageContainer.scrollTop = messageContainer.scrollHeight;
+  }
+
+  componentDidMount() {
+    this.scrollToBottom();
+  }
+
+  componentDidUpdate() {
+    this.scrollToBottom();
+  }
+
+  render() {
+    const { partner, messages, onCloseConversation } = this.props;
+    return (
+      <div className="conversation"> 
+        <div>
+          <div className="conversation-header">
+            <div className="partner-info">
+              <img className="user-profile-photo" src={partner.profilePhotoUrl} />
+              <h3 className="user-title">{partner.name}</h3>
+            </div>
+            <Icon onClick={onCloseConversation} size="large" name="close" />
+          </div>
         </div>
-        <Icon onClick={onCloseConversation} size="large" name="close" />
+    
+        <div id="conversation-body">
+          {
+            messages.map(message => <Message content={message.content} fromPartner={message.from === partner.id} />)
+          }
+        </div>
+    
+        <div className="message-input-container">
+          <input className="message-input" type="text" />
+        </div>
       </div>
-    </div>
-
-    <div className="conversation-body">
-      {
-        messages.map(message => <Message content={message.content} fromPartner={message.from === partner.id} />)
-      }
-    </div>
-
-    <div className="message-input-container">
-      <input className="message-input" type="text" />
-    </div>
-  </div>
-);
+    );
+  }
+};
 
 const mapStateToProps = ({ conversation }) => ({
   partner: conversation.partner,
