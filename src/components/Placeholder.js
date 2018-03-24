0cdde85ec5015490e6a302225b0e8eec5f5313de
Ke Nguyen <ke.nguyen@2359media.com>
2018-03-24 15:16:41 +0700

    feat(ui): add Placeholder component

---
+++ src/components/Placeholder.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Icon } from 'semantic-ui-react';
+
+const Placeholer = () => (
+  <div style={{ padding: '20 0%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
+    <Icon size="massive" color="violet" name="comments outline"></Icon>
+    <h2>Select a friend to start chatting!!!</h2>
+  </div>
+);
+
+export default Placeholer;
