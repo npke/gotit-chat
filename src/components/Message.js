f4734dd57b76e22fce76f97b9124bc341085c77e
Ke Nguyen <ke.nguyen@2359media.com>
2018-03-25 00:35:19 +0700

    feat(ui): add css for system message

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './Message.css';
 
-const Message = ({ content, fromPartner }) => {
-  const messageClassNames = ["message-container", fromPartner ? "message-container-left" : "message-container-right"];
+const Message = ({ content, from }) => {
+  const messageClassNames = ["message-container", from === "partner" ? "message-container-left" : from === "system" ? "system-message" : "message-container-right"];
   return (
     <div className={messageClassNames.join(" ")}>
       <div className="message-item">
