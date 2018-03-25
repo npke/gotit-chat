c1731962ac61004a8a14cf4d6de7fa31ac2a709b
Ke Nguyen <ke.nguyen@2359media.com>
2018-03-25 10:29:06 +0700

    feat(message): escape html characters to prevent XSS

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import Validator from 'validator';
 import './Message.css';
 
 const formatMessage = (message) => {
-  return message.replace(/(https?:\/\/[^\s]+)/gi, "<a target='_blank' href='$1'>$1</a>")
-    .replace(/:\)/gi, '\u{1F642}')
+  const emojiFormatMessage =  message.replace(/:\)/gi, '\u{1F642}')
     .replace(/:D/gi, '\u{1F603}')	
     .replace(/:p/gi, '\u{1F60B}')	
     .replace(/:'\)/gi, '\u{1F602}')	
     .replace(/:-\*/gi, '\u{1F618}')	
     .replace(/:\(/gi, '\u{1F61E}')	
     .replace(/:'\(/gi, '\u{1F622}');
+
+  const escapedMessage = Validator.escape(emojiFormatMessage);
+
+  return escapedMessage.replace(/(https?:(&#x2F;){2}[^\s]+)/gi, "<a target='_blank' href='$1'>$1</a>");
 }
 
 const Message = ({ content, from }) => {
