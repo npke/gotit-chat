1b7c9f2c27864053ef861a771196c0390d8afe1c
Ke Nguyen <ke.nguyen@2359media.com>
2018-03-25 18:06:11 +0700

    fix(user list): fix not show scrollbar when content overflow

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -5,15 +5,13 @@ import User from './User';
 import "./UserList.css";
 
 const UserList = ({ users, onSelectUser, isLoading }) => (
-  <div>
-    <ul className="user-list">
-      {
-        isLoading
-        ? <Segment style={{ padding: '100px' }} basic loading></Segment>
-        : users.map((user) => <User onSelect={onSelectUser} key={user.id} {...user} />)
-      }
-    </ul>
-  </div>
+  <ul className="user-list">
+    {
+      isLoading
+      ? <Segment style={{ padding: '100px' }} basic loading></Segment>
+      : users.map((user) => <User onSelect={onSelectUser} key={user.id} {...user} />)
+    }
+  </ul>
 );
 
 export default UserList;
