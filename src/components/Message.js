2d6f1715a433b489c0aa5d0d3b4795e8d34f2e27
Ke Nguyen <ke.nguyen@2359media.com>
2018-03-25 09:57:36 +0700

    feat(message): format link and emoji icon

--- src/components/Message.js
+++ src/components/Message.js
@@ -2,7 +2,14 @@ import React from 'react';
 import './Message.css';
 
 const formatMessage = (message) => {
-  return message.replace(/(https?:\/\/[^\s]+)/gi, "<a target='_blank' href='$1'>$1</a>");
+  return message.replace(/(https?:\/\/[^\s]+)/gi, "<a target='_blank' href='$1'>$1</a>")
+    .replace(/:\)/gi, '\u{1F642}')
+    .replace(/:D/gi, '\u{1F603}')	
+    .replace(/:p/gi, '\u{1F60B}')	
+    .replace(/:'\)/gi, '\u{1F602}')	
+    .replace(/:-\*/gi, '\u{1F618}')	
+    .replace(/:\(/gi, '\u{1F61E}')	
+    .replace(/:'\(/gi, '\u{1F622}');
 }
 
 const Message = ({ content, from }) => {
